fc76e69cf1fc9c900d58889c1e12396434badfd6
Samee <sameeullah03@gmail.com>
2023-12-14 14:38:26 +0500

    Laravel breeze setup done.

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,20 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
     ],
+    server: {
+        host: 'local.lara-10.com',
+        proxy: {
+            '/api': {
+                target: 'http://local.lara-10.com',
+                changeOrigin: true,
+            },
+        },
+    }
 });
